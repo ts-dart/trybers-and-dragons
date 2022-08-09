279a19901fcb1175e70a5b256d8739f8d64bad67
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-08-09 16:59:10 -0300

    feat: foi criada a classe Races

---
+++ src/Races/Races.ts
@@ -0,0 +1,15 @@
+export default abstract class Races {
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) {}
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
